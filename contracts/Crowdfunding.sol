3ae137507d61f91b406f2fb5a49cedfb0435a743
Kiranraj27 <kiranraj586@gmail.com>
2023-08-20 11:47:14 +0530

    Contract completed

--- contracts/Crowdfunding.sol
+++ contracts/Crowdfunding.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract Crowdfunding {
+contract CrowdFunding {
     struct Campaign {
         address owner;
         string title;
@@ -18,10 +18,10 @@ contract Crowdfunding {
 
     uint256 public numberOfCampaigns = 0;
 
-    function createCampaign(address _owner, string memory _title,string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns (uint256){
+    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns (uint256) {
         Campaign storage campaign = campaigns[numberOfCampaigns];
 
-        require(campaign.deadline < block.timestamp,"The deadline should be date in the future");
+        require(campaign.deadline < block.timestamp, "The deadline should be a date in the future.");
 
         campaign.owner = _owner;
         campaign.title = _title;
@@ -33,12 +33,37 @@ contract Crowdfunding {
 
         numberOfCampaigns++;
 
-        return numberOfCampaigns - 1; 
+        return numberOfCampaigns - 1;
     }
 
-    function donateCampaign(){}
+    function donateToCampaign(uint256 _id) public payable {
+        uint256 amount = msg.value;
 
-    function getDonators(){}
+        Campaign storage campaign = campaigns[_id];
 
-    function getCampaigns(){}
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+
+        (bool sent,) = payable(campaign.owner).call{value: amount}("");
+
+        if(sent) {
+            campaign.amountCollected = campaign.amountCollected + amount;
+        }
+    }
+
+    function getDonators(uint256 _id) view public returns (address[] memory, uint256[] memory) {
+        return (campaigns[_id].donators, campaigns[_id].donations);
+    }
+
+    function getCampaigns() public view returns (Campaign[] memory) {
+        Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
+
+        for(uint i = 0; i < numberOfCampaigns; i++) {
+            Campaign storage item = campaigns[i];
+
+            allCampaigns[i] = item;
+        }
+
+        return allCampaigns;
+    }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,38 +1,15 @@
-require("@matterlabs/hardhat-zksync-solc");
-
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  zksolc: {
-    version: "1.3.9",
-    compilerSource: "binary",
-    settings: {
-      optimizer: {
-        enabled: true,
-      },
-    },
-  },
-  networks: {
-    zksync_testnet: {
-      url: "https://zksync2-testnet.zksync.dev",
-      ethNetwork: "goerli",
-      chainId: 280,
-      zksync: true,
-    },
-    zksync_mainnet: {
-      url: "https://zksync2-mainnet.zksync.io/",
-      ethNetwork: "mainnet",
-      chainId: 324,
-      zksync: true,
-    },
-  },
-  paths: {
-    artifacts: "./artifacts-zk",
-    cache: "./cache-zk",
-    sources: "./contracts",
-    tests: "./test",
-  },
   solidity: {
-    version: "0.8.17",
+    version: '0.8.9',
+    defaultNetwork: 'sepolia',
+    networks: {
+      hardhat: {},
+      goerli: {
+        url: 'https://rpc.sepolia.org',
+        accounts: [`0x${process.env.PRIVATE_KEY}`]
+      }
+    },
     settings: {
       optimizer: {
         enabled: true,
